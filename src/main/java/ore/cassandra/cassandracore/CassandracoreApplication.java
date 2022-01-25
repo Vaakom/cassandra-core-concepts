e837b7689b4dab4f5a5460068b114b4765db2ce3
Vaakom <systemplast@yandex.ru>
2022-01-25 10:08:53 +0300

    Let's start

---
+++ src/main/java/ore/cassandra/cassandracore/CassandracoreApplication.java
@@ -0,0 +1,13 @@
+package ore.cassandra.cassandracore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandracoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CassandracoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ore/cassandra/cassandracore/CassandracoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ore.cassandra.cassandracore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CassandracoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
